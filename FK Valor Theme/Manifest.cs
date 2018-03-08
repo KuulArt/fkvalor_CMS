082fc5155ed05f9d9fb89de9037853655c5b12c4
Toms Lisments <toms.lisments@live.com>
2018-03-09 00:27:24 +0200

    Almost done with the blog summary view, homepage

--- FK Valor Theme/Manifest.cs
+++
@@ -1,9 +0,0 @@
-﻿using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "FK Valor Theme",
-    Author = "Toms Lisments",
-    Website = "http://fkvalor.lv",
-    Version = "2.0.0",
-    Description = ""
-)]
