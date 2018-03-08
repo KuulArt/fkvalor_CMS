6aa816cef9b0f448cce14032348c614c3174e7d0
Toms Lisments <toms.lisments@live.com>
2018-03-08 19:40:56 +0200

    Started on header

--- FKValorTheme/wwwroot/webpack.config.js
+++ FKValorTheme/wwwroot/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/js/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'static')
+        path: path.resolve(__dirname, 'js')
     },
     resolve: {
         modules: [
