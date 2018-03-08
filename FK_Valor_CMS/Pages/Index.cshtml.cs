f32296d0bc4ca40ee0d5080c438054c59c2e3469
Toms Lisments <toms.lisments@live.com>
2018-03-08 16:23:58 +0200

    Initial commit

---
+++ FK_Valor_CMS/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FK_Valor_CMS.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
