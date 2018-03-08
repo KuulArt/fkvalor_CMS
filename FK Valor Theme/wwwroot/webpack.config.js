21ef395b1908563e7158d86e1d82c3e8c6b87334
Toms Lisments <toms.lisments@live.com>
2018-03-08 17:20:37 +0200

    Get assets working

---
+++ FK Valor Theme/wwwroot/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'static')
+    },
+    resolve: {
+        modules: [
+            'node_modules',
+        ],
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }
+        }]
+    }
+};
---
+++ FKValorTheme/Manifest.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "FK Valor Theme",
+    Author = "Toms Lisments",
+    Website = "http://fkvalor.lv",
+    Version = "2.0.0",
+    Description = ""
+)]
