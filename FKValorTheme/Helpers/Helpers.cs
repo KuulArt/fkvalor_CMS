c8644491eb8b9513f4b3a9771f64673482958910
Toms Lisments <toms.lisments@live.com>
2018-04-13 19:27:58 +0300

    Update with Trenin page type template, add new fonts and styling improvements

---
+++ FKValorTheme/Helpers/Helpers.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using OrchardCore.ContentManagement;
+
+namespace FKValorTheme.Helpers
+{
+    public static class Helpers
+    {
+        public static string GetImagePath(object imageContent, string width = "500", string height = "300")
+        {
+            if (!(imageContent is JObject jObject)) return null;
+            var contentItem = jObject.ToObject<ContentItem>();
+            return "/media/" + contentItem.Content.Paths[0] + "?width=" + width + "&height=" + height;
+        }
+    }
+}
