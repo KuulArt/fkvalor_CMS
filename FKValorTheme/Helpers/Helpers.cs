b403f3d4d10ba288223540f3cbebc75420b2568e
Toms Lisments <toms.lisments@live.com>
2018-04-17 23:24:55 +0300

    Some styling adjustments

--- FKValorTheme/Helpers/Helpers.cs
+++ FKValorTheme/Helpers/Helpers.cs
@@ -5,11 +5,18 @@ namespace FKValorTheme.Helpers
 {
     public static class Helpers
     {
-        public static string GetImagePath(object imageContent, string width = "500", string height = "300")
+        public static string GetImagePath(object imageContent, string width, string height)
         {
             if (!(imageContent is JObject jObject)) return null;
             var contentItem = jObject.ToObject<ContentItem>();
             return "/media/" + contentItem.Content.Paths[0] + "?width=" + width + "&height=" + height;
+        }
+        
+        public static string GetImagePath(object imageContent)
+        {
+            if (!(imageContent is JObject jObject)) return null;
+            var contentItem = jObject.ToObject<ContentItem>();
+            return "/media/" + contentItem.Content.Paths[0];
         }
     }
 }
